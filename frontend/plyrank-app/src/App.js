61e82b65fb9ed4c5565f9ce5f59f7f84e458bce9
payalmantri <payaljm2@illinois.edu>
2023-04-07 15:57:18 -0500

    Add API to connect to backend

--- frontend/plyrank-app/src/App.js
+++ frontend/plyrank-app/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
-import About from './pages/about';
 import Events from './pages/events';
 import  Blogs from './pages/blogs';
 import Login from './Login';
@@ -17,7 +16,6 @@ function App() {
     <Routes>
     <Route path='/' exact element={<Login/>} />
          <Route path='/Home' exact element={<Home />} />
-         <Route path='/about' element={<About />} />
          <Route path='/events' element={<Events />} />
     
        
--- frontend/plyrank-app/src/pages/index.js
+++ frontend/plyrank-app/src/pages/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-  
+import PlayerList from './playerList';
+
 const Home = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
 
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-         <Navbar />
+    <div>
+      <Navbar />
       <h1>Welcome to GeeksforGeeks</h1>
+      <PlayerList></PlayerList>
     </div>
   );
 };
-  
+
 export default Home;
