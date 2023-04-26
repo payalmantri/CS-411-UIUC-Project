5fb64092f0875331ce5342ff367574179b4a27d0
payalmantri <payaljm2@illinois.edu>
2023-04-26 16:27:30 -0500

    Add users page

--- frontend/plyrank-app/src/MainLayoutRoutes.js
+++ frontend/plyrank-app/src/MainLayoutRoutes.js
@@ -6,6 +6,7 @@ import PlayerList from './pages/playerList';
 import TeamDetails from './pages/teamDetails';
 import Navbar from './components/Navbar';
 import TournamentList from './pages/tournamentList';
+import AdminPage from './pages/adminPage';
 
 
 
@@ -17,10 +18,9 @@ function MainLayoutRoutes() {
             <Routes>
                 <Route path='/Home' element={<TeamDetails />} />
                 <Route path='/ClubDetails' element={<ClubDetails />} />
-              
                 <Route path='/PlayerSearch' element={<PlayerList />} />
                 <Route path='/TournamentList' element={<TournamentList />} />
-                {/* <Route path="/Admin" element={<Admin />} /> */}
+                <Route path="/Admin" element={<AdminPage />} />
             </Routes>
        
         </div>
